aafb88864d2988bbc0c273b2a5f1236fd7229e4b
walkunderrain <39767467+walkunderrain@users.noreply.github.com>
2018-09-16 21:45:18 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,25 +1,44 @@
 module.exports =
 function solveEquation(equation) {
-// equation.replace(/\s+/g, ''));
+equation.replace(/\s+/g, '');
 var first = equation.replace(/\s+/g, '').split('*x^2');
-// console.log('first: ', first);
-//console.log(first);
-var second = first[1].split('*x');
-//console.log(second);
-var result = [];
-result.push(+first[0]);
-result.push(+second[0]);
-result.push(+second[1]);
 
-//console.log(result);
-var D = result[1]^2 - 4*result[0]*result[2];
-var sol1 = (-result[1] + Math.sqrt(D))/(2*result[0]);
-//console.log(D);
-var sol2 = (-result[1] - Math.sqrt(D))/(2*result[0]);
-  var solutions = [];
-  solutions.push(+sol1);
-  solutions.push(+sol2);
-  return solutions;
+// console.log('first: ', first);
+var second = first[1].split('*x');
+// console.log('second: ', second);
+// var result = [];
+// result.push(parseInt(first[0]));
+// result.push(parseInt(second[0]));
+// result.push(parseInt(second[1]));
+var a = parseInt(first[0]);
+var b = parseInt(second[0]);
+var c = parseInt(second[1]);
+
+
+
+// console.log('result: ', result);
+// console.log('a = ', a, 'b = ', b, 'c = ', c);
+// var D = result[1]^2 - 4*result[0]*result[2];
+var D = b**2 - a*c*4;
+// var sol1 = ((-1) * result[1] + Math.sqrt(D))/(2*result[0]);
+var x1 = (b - Math.sqrt(D))*(-1) / (a*2);
+var x2 = (b + Math.sqrt(D))*(-1) / (a*2);
+// var sol2 = ((-1)*result[1] - Math.sqrt(D))/(2*result[0]);
+// console.log('x1: ', x1);
+// console.log('x2: ', x2);
+var solutions = [];
+  solutions.push(Math.round(x1));
+  solutions.push(Math.round(x2));
+  // console.log('solutions: ', solutions);
+  return solutions.sort((a,b) => {
+    if (a !== b) {
+      if (a < b) {
+         return -1;
+       } else return 1;
+     } else return 0;
+    //(a !== b) ? ((a < b) ? -1 : 1) : 0;
+  });
 }
 
-// console.log(solveEquation('5 * x ^ 2 + 10 * x - 12'));
+// solveEquation('1 * x ^ 2 + 2 * x - 4');
+// solveEquation('294 * x^2 - 141195558 * x - 1600964090384736');
