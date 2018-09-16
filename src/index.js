a05415cfcfa0423852fc49a6f15f70cb156f720c
walkunderrain <39767467+walkunderrain@users.noreply.github.com>
2018-09-16 14:13:25 +0300

    Initial

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-module.exports = function solveEquation(equation) {
-  // your implementation
+module.exports =
+function solveEquation(equation) {
+// equation.replace(/\s+/g, ''));
+var first = equation.replace(/\s+/g, '').split('*x^2');
+// console.log('first: ', first);
+//console.log(first);
+var second = first[1].split('*x');
+//console.log(second);
+var result = [];
+result.push(+first[0]);
+result.push(+second[0]);
+result.push(+second[1]);
+
+//console.log(result);
+var D = result[1]^2 - 4*result[0]*result[2];
+var sol1 = (-result[1] + Math.sqrt(D))/(2*result[0]);
+//console.log(D);
+var sol2 = (-result[1] - Math.sqrt(D))/(2*result[0]);
+  var solutions = [];
+  solutions.push(+sol1);
+  solutions.push(+sol2);
+  return solutions;
 }
+
+// console.log(solveEquation('5 * x ^ 2 + 10 * x - 12'));
